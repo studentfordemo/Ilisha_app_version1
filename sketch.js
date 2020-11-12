60218cd93e91deb739050093eb19d51aa867b92b
studentfordemo <64936561+studentfordemo@users.noreply.github.com>
2020-11-12 13:27:04 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,9 +2,15 @@ var gameState = "homeScreen";
 var start, startimage, logo, logoimage;
 var help, helpImage;
 var ok, okImage;
-var inputBox;
+//var inputBox;
 var go, goImage;
 var string, string1, string2;
+var sound1;
+var mumbai, mButton;
+var bangalore, bButton;
+var hyderabad, hButton;
+var delhi, dButton;
+
 
 function preload(){
    startimage = loadImage("StartButton.png");
@@ -12,20 +18,26 @@ function preload(){
    helpimage = loadImage("HelpButton.png");
    okImage = loadImage("OkButton.png");
    goImage = loadImage("GoButton.png");
+   sound1 = loadSound("piano_D_sharp.mp3");
+   mButton = loadImage("MUMBAI.jpg");
+   bButton = loadImage("BANGALORE1.jpg");
+   hButton = loadImage("HYDERABAD.jpg");
+   dButton = loadImage("DELHI.jpg");
 }
 
 function setup() {
-  createCanvas(windowWidth,windowHeight);
+  createCanvas(800,800);
   logo = createSprite(400, 250, 1, 1);
   start = createSprite(400, 450, 1, 1);
   help = createSprite(750, 50, 1, 1);
   ok = createSprite(400, 600, 1, 1);
   go = createSprite(440, 600, 1, 1);
-  inputBox = createInput();
+ 
 }
 
 function draw() {
   background("#F70000");  
+  
   if (gameState === "homeScreen" ){
     textSize(35);
     fill("cyan");
@@ -33,9 +45,9 @@ function draw() {
     text("Book Donation", 300, 100);
     logo.addImage(logoimage);
     start.addImage(startimage);
-    if (mousePressedOver(start)||touches.length > 0 ){
+    if (mousePressedOver(start)){
       gameState = "screen1";
-      touches = [];
+      sound1.play();
     }
   }
   if (gameState === "screen1"){
@@ -47,60 +59,74 @@ function draw() {
     help.addImage(helpimage);
     help.scale = 0.3;
     ok.visible = false;
-    inputBox.position(350, 350); 
-    textSize(18);
-    fill("red");
-    go.addImage(goImage);
-
-    string = inputBox.value();
-    string1 = string.toLowerCase();
-    string2 = string.toUpperCase();
+    mumbai = createSprite(100, 100, 1, 1);
+  
+    hyderabad = createSprite(110, 250, 1, 1);
+    hyderabad.scale=1.1
+    bangalore = createSprite(100, 385, 1, 1);
+   
+    delhi = createSprite(100, 530, 1, 1);
+   
+    hyderabad.addImage(hButton);
+    delhi.addImage(dButton);
+    bangalore.addImage(bButton);
+    mumbai.addImage(mButton);
     
-    if ((mousePressedOver(go) ||touches.length > 0) && (inputBox.value()==="Mumbai" || string1 === "mumbai" || string2 === "MUMBAI")){
+    if (mousePressedOver(mumbai)){
       gameState = "MumbaiDetails";
-      touches = [];
     }
 
-    if ((mousePressedOver(go) ||touches.length > 0)  && (inputBox.value()==="Hyderabad" || string1 === "hyderabad" || string2 === "HYDERABAD")){
+    if (mousePressedOver(hyderabad)){
       gameState = "HyderabadDetails";
-      touches = [];
     }
 
-    if ((mousePressedOver(go) ||touches.length > 0) && (inputBox.value()==="Bangalore" || string1 === "bangalore" || string2 === "BANGALORE")){
+    if (mousePressedOver(bangalore)){
       gameState = "BangaloreDetails";
-      touches = [];
     }
 
-    if ((mousePressedOver(go) ||touches.length > 0) && (inputBox.value()==="Delhi" || string1 === "delhi" || string2 === "DELHI")){
+    if (mousePressedOver(delhi)){
       gameState = "DelhiDetails";
-      touches = [];
     }
 
-    text("Please Enter Your Location:", 340, 300);
-    if (mousePressedOver(help) ||touches.length > 0){
+    //text("Please Enter Your Location:", 340, 300);
+    if (mousePressedOver(help)){
       background("#3594C8");
       gameState = "helpScreen";
-      touches = [];
     }
   }
   if (gameState === "helpScreen"){
     go.visible = false;
     ok.visible = true;
-    inputBox.position(12000, 800);
+    //inputBox.position(12000, 800);
     textSize(20);
     background("#3594C8");
     help.visible = false;
-    text("This is a book donation app, dedicated to educating and boosting", 150, 220)
-    text("the creative skills of children in orphanages. Everyone deserves", 150, 240);
-    text("a chance to show what they are capable of. This is a simple app", 150, 260); 
-    text("where, by just typing your location, you can see local orphanages", 150, 280);
-    text("in your area and donate some books to the children there.", 150, 300);
+    fill("white")
+    textFont("Brush Script MT")
+    text("This is a book donation app, dedicated to educating and boosting", 200, 220)
+    text("the creative skills of children in orphanages. Everyone deserves", 200, 240);
+    text("a chance to show what they are capable of. This is a simple app", 200, 260); 
+    text("where, by just typing your location, you can see local orphanages", 200, 280);
+    text("in your area and donate some books to the children there.", 200, 300);
     ok.addImage(okImage);
     ok.scale = 0.3;
-    if (mousePressedOver(ok)||touches.length > 0){
+    if (mousePressedOver(ok)){
       gameState = "screen1";
-      touches = [];
     }
+  }
+  if (gameState === "MumbaiDetails"){
+    
+    //text("Address: Veera Desai Road, Prathmesh Complex, Andheri(W, Mumbai, Maharashtra 400058", 100, 120) 
+    //text("n", 100, 130);
+    //text("Phone: 022 2624 1109", 100, 150);
+    
+    MumbaiDetails();
+    if (mousePressedOver(help)){
+      background("#F70000");
+      gameState = "helpScreen";
+    }
+    go.visible = false;
+    //inputBox.hide();
   }
 
   if (gameState === "HyderabadDetails"){
@@ -116,20 +142,7 @@ function draw() {
 
   drawSprites();
   
-  if (gameState === "MumbaiDetails"){
-    
-    //text("Address: Veera Desai Road, Prathmesh Complex, Andheri(W, Mumbai, Maharashtra 400058", 100, 120) 
-    //text("n", 100, 130);
-    //text("Phone: 022 2624 1109", 100, 150);
-    
-    MumbaiDetails();
-    if (mousePressedOver(help)){
-      background("#F70000");
-      gameState = "helpScreen";
-    }
-    go.visible = false;
-    inputBox.hide();
-  }
+  
 }
 
 function hide(){
@@ -137,7 +150,7 @@ function hide(){
 }
 
 function MumbaiDetails(){
-  var response = window.location.assign("https://ilisha0512.github.io/mDetails/");
+  var response = window.location.assign("https://ilisha0512.github.io/Mumbai-Orphanages-Details/");
   console.log(response);
 }
 
